89c0f26a3f27d72da9dc68685da8e80c08e1ddd3
SantiBalbiani <santiagobalbiani@yahoo.com>
2015-06-22 17:57:57 -0300

    Se crea clase solicitante
    Solicitante es aquel que se está creando un usuario

---
+++ Solicitante.class.php
@@ -0,0 +1,29 @@
+<?php
+
+
+/**
+ * 
+ */
+class Solicitante {
+
+    /**
+     * 
+     */
+    public function __construct() {
+    }
+
+    /**
+     * @var void
+     */
+    public $Attribute1;
+
+    /**
+     * @param void $ElSistema 
+     * @return unUsuario
+     */
+    public function crearUsuarioEn($ElSistema) {
+        // TODO implement here
+        return null;
+    }
+
+}
