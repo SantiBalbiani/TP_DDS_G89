1bc188f11d426549de330d5d189780dd0e28a256
SantiBalbiani <santiagobalbiani@yahoo.com>
2015-06-22 17:56:21 -0300

    Se crea Clase Receta
    Se crea Clase Receta

---
+++ Receta.class.php
@@ -0,0 +1,28 @@
+<?php
+
+
+/**
+ * 
+ */
+class Receta {
+
+    /**
+     * 
+     */
+    public function __construct() {
+    }
+
+    /**
+     * @var void
+     */
+    public $categorias;
+
+    /**
+     * @var void
+     */
+    public $calificacion;
+
+
+
+
+}
