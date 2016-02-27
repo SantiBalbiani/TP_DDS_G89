4375b8c5d94f8363a3abefa9246725b4b0c1e96d
Chespy <emiliano.gsuchecki@gmail.com>
2016-02-27 02:12:29 -0300

    vuelvo a subir el TestPlanificacion

--- src/Testeo/TestPlanificacion.java
+++ src/Testeo/TestPlanificacion.java
@@ -18,119 +18,121 @@ import usuario.Usuario;
 public class TestPlanificacion {
 
 	
-//	@Test
-//	public void testPlanificacion() {
-//		
-//		Planificacion unaPlanificacion = new Planificacion();
-//		
-//		Usuario unUser = new Usuario();
-//		
-//		//++++++++++++++++++Usuario se Loguea++++++++++++++++++++++++++
-//		unUser = unUser.buscarUsuarioPorNombre("HOLAMANUEL");
-//		
-//		Receta unaReceta = new Receta();
-//		
-//		
-//		//+++++++++++++++++++++Receta asociada a usuario con intencion de planificar+++++++++++
-//		unaReceta = (Receta)unUser.getRecetasUser().iterator().next();
-//		
-//		TipoReceta unTipo = new TipoReceta();
-//		
-//		Session sessionHIB = HibernateConf.getSessionFactory().openSession();
-//
-//		Query query = sessionHIB.createQuery("FROM TipoReceta e where e.tipoReceta = :tipoReceta");
-//
-//		//++++++++++++++++++++El usuario decide poner la receta para desayuno++++++++++++++++++
-//		query.setString("tipoReceta", "Desayuno");
-//		
-//		try {
-//			java.util.List<?> lista;
-//			
-//			
-//			
-//			lista = query.list();
-//			
-//
-//				unTipo = (TipoReceta)lista.get(0);
-//				
-//				
-//		
-//		}catch(Throwable theException) 	    
-//		
-//	{
-//	     System.out.println("algo paso"); 
-//	}
-//		
-//		
-//	sessionHIB.close();
-////+++++++++++++++++++++++++++Se dirige al boton crear Planificacion+++++++++++++++
-//		
-//		
-//		sessionHIB = HibernateConf.getSessionFactory().openSession();
-//		sessionHIB.beginTransaction();
-//		
-//		String fechaEnString = "24/02/2016";
-//		
-//		   SimpleDateFormat formatter = new SimpleDateFormat("dd/MM/yyyy"); //;
-// 
-//			Date date = new Date();
-//			try {
-//				date = formatter.parse(fechaEnString);
-//			} catch (ParseException e) {
-//				 
-//				e.printStackTrace();
-//			}
-//			
-//			
-//		
-//	unaPlanificacion.setFecha(date);
-//	
-//	unaPlanificacion.setUsuario(unUser);
-//	
-//	unaPlanificacion.setReceta(unaReceta);
-//	
-//	unaPlanificacion.setUnTipoDeReceta(unTipo);
-//	
-//	sessionHIB.save(unaPlanificacion);
-//	
-//	sessionHIB.getTransaction().commit();
-//	System.out.println("Done");
-//	sessionHIB.close();
-//	
-//		
-//		
-//		// TODO Auto-generated constructor stub
-//	}
-//	@Test
-//	public void TestPlanifXUser(){
-//		
-//		
-//	Usuario unUser = new Usuario();
-//		
-//		//++++++++++++++++++Usuario se Loguea++++++++++++++++++++++++++
-//		unUser = unUser.buscarUsuarioPorNombre("HOLAMANUEL");
-//			
-//			Session sessionHIB = HibernateConf.getSessionFactory().openSession();
-//
-//			Query query = sessionHIB.createQuery("FROM Planificacion p WHERE p.usuario = :usuario");
-//			
-//			query.setParameter("usuario", unUser);
-//			
-//			@SuppressWarnings("unchecked")
-//			List<Planificacion> listaPlanifXUser = query.list();
-//
-//			sessionHIB.close();
-//			
-//			for (Planificacion unaPl :listaPlanifXUser){
-//				
-//				System.out.println(unaPl.getFecha());
-//				System.out.println(unaPl.getReceta().getNombreReceta());
-//				System.out.println(unaPl.getUnTipoDeReceta().getTipoReceta());
-//				
-//				
-//				
-//			
-//		}
-//	}
-//	
+	@Test
+	public void testPlanificacion() {
+		
+		Planificacion unaPlanificacion = new Planificacion();
+		
+		Usuario unUser = new Usuario();
+		
+		//++++++++++++++++++Usuario se Loguea++++++++++++++++++++++++++
+		//TODO: cambiar el nombre de usuario por el de c/u
+		unUser = unUser.buscarUsuarioPorNombre("HOLAMANUEL");
+		
+		Receta unaReceta = new Receta();
+		
+		
+		//+++++++++++++++++++++Receta asociada a usuario con intencion de planificar+++++++++++
+		unaReceta = (Receta)unUser.getRecetasUser().iterator().next();
+		
+		TipoReceta unTipo = new TipoReceta();
+		
+		Session sessionHIB = HibernateConf.getSessionFactory().openSession();
+
+		Query query = sessionHIB.createQuery("FROM TipoReceta e where e.tipoReceta = :tipoReceta");
+
+		//++++++++++++++++++++El usuario decide poner la receta para desayuno++++++++++++++++++
+		query.setString("tipoReceta", "Desayuno");
+		
+		try {
+			java.util.List<?> lista;
+			
+			
+			
+			lista = query.list();
+			
+
+				unTipo = (TipoReceta)lista.get(0);
+				
+				
+		
+		}catch(Throwable theException) 	    
+		
+	{
+	     System.out.println("algo paso"); 
+	}
+		
+		
+	sessionHIB.close();
+//+++++++++++++++++++++++++++Se dirige al boton crear Planificacion+++++++++++++++
+		
+		
+		sessionHIB = HibernateConf.getSessionFactory().openSession();
+		sessionHIB.beginTransaction();
+		
+		String fechaEnString = "24/02/2016";
+		
+		   SimpleDateFormat formatter = new SimpleDateFormat("dd/MM/yyyy"); //;
+ 
+			Date date = new Date();
+			try {
+				date = formatter.parse(fechaEnString);
+			} catch (ParseException e) {
+				 
+				e.printStackTrace();
+			}
+			
+			
+		
+	unaPlanificacion.setFecha(date);
+	
+	unaPlanificacion.setUsuario(unUser);
+	
+	unaPlanificacion.setReceta(unaReceta);
+	
+	unaPlanificacion.setUnTipoDeReceta(unTipo);
+	
+	sessionHIB.save(unaPlanificacion);
+	
+	sessionHIB.getTransaction().commit();
+	System.out.println("Done");
+	sessionHIB.close();
+	
+		
+		
+		// TODO Auto-generated constructor stub
+	}
+	@Test
+	public void TestPlanifXUser(){
+		
+		
+	Usuario unUser = new Usuario();
+		
+		//++++++++++++++++++Usuario se Loguea++++++++++++++++++++++++++
+		//TODO: cambiar el nombre de usuario por el de c/u
+		unUser = unUser.buscarUsuarioPorNombre("HOLAMANUEL");
+			
+			Session sessionHIB = HibernateConf.getSessionFactory().openSession();
+
+			Query query = sessionHIB.createQuery("FROM Planificacion p WHERE p.usuario = :usuario");
+			
+			query.setParameter("usuario", unUser);
+			
+			@SuppressWarnings("unchecked")
+			List<Planificacion> listaPlanifXUser = query.list();
+
+			sessionHIB.close();
+			
+			for (Planificacion unaPl :listaPlanifXUser){
+				
+				System.out.println(unaPl.getFecha());
+				System.out.println(unaPl.getReceta().getNombreReceta());
+				System.out.println(unaPl.getUnTipoDeReceta().getTipoReceta());
+				
+				
+				
+			
+		}
+	}
+	
  }
